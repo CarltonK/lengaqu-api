a60486aec38da3d37541ed486076b7d23d37d312
Mark Carlton <mcarlton33@gmail.com>
2022-07-18 18:58:27 +0300

    LQ-1 #comment lapi gcloud config

--- cloudbuild/scripts/deploy-cloud-run.sh
+++ cloudbuild/scripts/deploy-cloud-run.sh
@@ -14,13 +14,12 @@ echo "* Variable subsitituion *"
 echo "************************"
 echo
 
-awk '{gsub(/<KAPI_CONTAINER_IMAGE>/,"gcr.io/'$2'/'$1':'$5'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
+awk '{gsub(/<LAPI_CONTAINER_IMAGE>/,"gcr.io/'$2'/'$1':'$5'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
 awk '{gsub(/<MIN_SCALE>/,"'$6'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
 awk '{gsub(/<MAX_SCALE>/,"'$7'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
-awk '{gsub(/<KAPI_SA>/,"'$8'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
+awk '{gsub(/<LAPI_SA>/,"'$8'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
 awk '{gsub(/<JWT_SECRET_KEY>/,"'$9'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
 awk '{gsub(/<DATABASE_INSTANCE_CONNECTION_NAME>/,"'$4'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
-awk '{gsub(/<GOOGLE_CLOUD_PROJECT>/,"'${10}'")}1' /workspace/service.yaml >/workspace/tmp.yaml && mv /workspace/tmp.yaml /workspace/service.yaml
 
 
 echo
